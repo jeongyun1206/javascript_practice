0009402899bf8f8ee15adbdeb1ef9e6df5b459cb
Jeongyun Nho <jnho@c2r6s2.42seoul.kr>
2023-07-08 18:12:34 +0900

    7/8 모듈화, 입출력 기본 보안 구현완료

--- web-2_node_js/main.js
+++ web-2_node_js/main.js
@@ -2,127 +2,27 @@ const http =require('http');
 const fs = require('fs').promises;
 const url = require('url');
 const qs = require('querystring');
-const { parse } = require('path');
-
-const getTitle = function(parsedUrl) {
-    if (!parsedUrl.query.id)
-        return ('welcome');
-    else
-        return (parsedUrl.query.id);
-}
-
-const getDescription = async function(title) {
-    if (title == 'welcome')
-        return ('Hello Node.js');
-    return (await fs.readFile(__dirname + '/Data/' + title, 'utf-8'));
-}
-
-const getList = async function() {
-    const dataList = await fs.readdir('./Data');
-    let list = '<ul>';
-    for (dataElement of dataList) {
-        list += `<li><a href="/?id=${dataElement}">${dataElement}</a></li>`
-    }
-    list += '</ul>'
-    return (list);
-}
-
-const getControl = function(title) {
-    if (title === 'welcome')
-        return ('<a href="/create">create</a>');
-    return (`<a href="/create">create</a> 
-    <a href="/update?id=${title}">update</a> 
-    <form action="delete_process" method="post"> 
-        <input type="hidden" name="id" value="${title}"> 
-        <input type="submit" value="delete"> 
-    </form>`);
-}
-
-const getBasicTemplate = async function(parsedUrl) {
-    const title = getTitle(parsedUrl);
-    const description = await getDescription(title);
-    const control = getControl(title);
-    const list = await getList();
-    const template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    ${control}
-                    <h2>${title}</h2>
-                    <p>
-                        ${description}
-                    </p>
-                    </body>
-                    </html>
-                    `
-    return (template);
-}
-
-const getForm = async function (title, type) {
-    let titleValue = '', description = '', updateInfo = '';
-    if (type === 'update') {
-        description = await fs.readFile('Data/' + title, 'utf-8');
-        titleValue = 'value=' + title;
-        updateInfo = `<input type="hidden" name="id" value="${title}" ></input>`;
-    }
-    const form = 
-    `<form action="http://localhost:8080/${type}_process" method="post">
-        ${updateInfo}
-        <p>
-            <input type="text" name="title" placeholder="title" ${titleValue}>
-        </p>
-        <p>
-            <textarea name="description" placeholder="description" >${description}</textarea>
-        </p>
-        <p>
-            <input type="submit">
-        </p>
-    </form>`
-    return (form);
-}
-
-const getFormTemplate = async function(parsedUrl, type) {
-    const title = getTitle(parsedUrl);
-    const form = await getForm(title, type);
-    const list = await getList();
-    const template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <p>${form}</p>
-                    </body>
-                    </html>
-                    `
-    return (template);
-}
+const Template = require('./template.js');
+const path = require('path');
 
 const server = http.createServer(async function (req, res) {
     try {
         let parsedUrl = url.parse(req.url, true);
         if (parsedUrl.pathname === '/') {
-            const basicTemplate = await getBasicTemplate(parsedUrl);
+            const template = new Template(parsedUrl);
+            await template.TemplateConstructor(parsedUrl);
             res.writeHead(200);
-            res.end(basicTemplate);
+            res.end(template.getTemplate());
         } else if (parsedUrl.pathname === '/create'){
-            const formTemplate = await getFormTemplate(parsedUrl, 'create');
+            const template = new Template(parsedUrl, 'create');
+            await template.TemplateConstructor(parsedUrl, 'create');
             res.writeHead(200);
-            res.end(formTemplate);
+            res.end(template.getTemplate());
         } else if (parsedUrl.pathname === '/update') {
-            const formTemplate = await getFormTemplate(parsedUrl, 'update');
+            const template = new Template(parsedUrl, 'update');
+            await template.TemplateConstructor(parsedUrl, 'update');
             res.writeHead(200);
-            res.end(formTemplate);
+            res.end(template.getTemplate());
         } else if (['/create_process', '/update_process', '/delete_process'].includes(parsedUrl.pathname)) {
             let body = '';
             req.on('data', (data) => {
@@ -130,18 +30,21 @@ const server = http.createServer(async function (req, res) {
             });
             req.on('end',  () => {
                 const post = qs.parse(body);
-                console.log(post);
                 if (parsedUrl.pathname === '/update_process') {
-                    fs.rename(`Data/${post.id}`, `Data/${post.title}`);
-                    fs.writeFile('Data/' + post.title, post.description, 'utf-8', (err) => { console.error(err); });
-                    res.writeHead(302, {Location: `/?id=${post.title}`});
+                    const filteredId = path.parse(post.id).base;
+                    const filteredTitle = path.parse(post.title).base;
+                    fs.rename(`Data/${filteredId}`, `Data/${filteredTitle}`);
+                    fs.writeFile('Data/' + filteredTitle, post.description, 'utf-8', (err) => { console.error(err); });
+                    res.writeHead(302, {Location: `/?id=${filteredTitle}`});
                 }
                 else if (parsedUrl.pathname === '/create_process') {
-                    fs.writeFile('Data/' + post.title, post.description, 'utf-8', (err) => { console.error(err); });
-                    res.writeHead(302, {Location: `/?id=${post.title}`});
+                    const filteredTitle = path.parse(post.title).base;
+                    fs.writeFile('Data/' + filteredTitle, post.description, 'utf-8', (err) => { console.error(err); });
+                    res.writeHead(302, {Location: `/?id=${filteredTitle}`});
                 }
                 else if (parsedUrl.pathname === '/delete_process') {
-                    fs.unlink(`Data/${post.id}`);
+                    const filteredId = path.parse(post.id).base;
+                    fs.unlink(`Data/${filteredId}`);
                     res.writeHead(302, {Location: `/`});
                 }
                 res.end();
---
+++ web-2_node_js/template.js
@@ -0,0 +1,109 @@
+const fs = require('fs').promises;
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
+
+class Template {
+    constructor(parsedUrl, method) {
+        this.TemplateConstructor(parsedUrl, method);
+    }
+    async TemplateConstructor(parsedUrl, method) {
+        this.title = sanitizeHtml(this.getTitle(parsedUrl));
+        this.list = await this.getList(parsedUrl);
+        if (!method) {
+            this.control = this.getControl(this.title);
+            this.description = await this.getDescription(this.title);
+            this.template = `
+                        <!doctype html>
+                        <html>
+                        <head>
+                        <title>WEB1 - ${this.title}</title>
+                        <meta charset="utf-8">
+                        </head>
+                        <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${this.list}
+                        ${this.control}
+                        <h2>${this.title}</h2>
+                        <p>
+                            ${sanitizeHtml(this.description)}
+                        </p>
+                        </body>
+                        </html>
+                        `
+        } else {
+            this.form = await this.getForm(method);
+            this.template = `
+                        <!doctype html>
+                        <html>
+                        <head>
+                        <title>WEB1 - ${this.title}</title>
+                        <meta charset="utf-8">
+                        </head>
+                        <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${this.list}
+                        <p>${this.form}</p>
+                        </body>
+                        </html>
+                        `
+
+        }
+    }
+    getTitle(parsedUrl) {
+        if (!parsedUrl.query.id)
+            return ('welcome');
+        else
+            return (path.parse(parsedUrl.query.id).base);
+    }
+    async getForm(method) {
+        let titleValue = '', description = '', updateInfo = '';
+        if (method === 'update') {
+            description = await fs.readFile('Data/' + this.title, 'utf-8');
+            titleValue = 'value=' + this.title;
+            updateInfo = `<input type="hidden" name="id" value="${this.title}" ></input>`;
+        }
+        const form = 
+        `<form action="http://localhost:8080/${method}_process" method="post">
+            ${updateInfo}
+            <p>
+                <input type="text" name="title" placeholder="title" ${titleValue}>
+            </p>
+            <p>
+                <textarea name="description" placeholder="description" >${description}</textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>`
+        return (form);
+    }
+    async getList() {
+        const dataList = await fs.readdir('./Data');
+        let list = '<ul>';
+        for (let dataElement of dataList) {
+            list += `<li><a href="/?id=${dataElement}">${dataElement}</a></li>`
+        }
+        list += '</ul>'
+        return (list);
+    }
+    async getDescription(title) {
+        if (title == 'welcome')
+            return ('Hello Node.js');
+        return (await fs.readFile(__dirname + '/Data/' + title, 'utf-8'));
+    }
+    getControl(title) {
+        if (title === 'welcome')
+            return ('<a href="/create">create</a>');
+        return (`<a href="/create">create</a> 
+        <a href="/update?id=${title}">update</a> 
+        <form action="delete_process" method="post"> 
+            <input type="hidden" name="id" value="${title}"> 
+            <input type="submit" value="delete"> 
+        </form>`);
+    }
+    getTemplate() {
+        return (this.template);
+    }
+};
+
+module.exports = Template;
